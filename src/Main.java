bc4bf7ca1f09c9a12b39d287d19b7fd8d7a3ccc8
Konstantin Chigridov <werest54@rambler.ru>
2023-03-31 20:02:02 +0300

    Пул потоков

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,31 @@
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        List<Thread> threadList = new ArrayList<>();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         String[] texts = new String[25]; //25 строк
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000); //каждая строка
         }
 
+        List<Future<Integer>> futureList = new ArrayList<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
         long startTs = System.currentTimeMillis(); // start time
         for (String text : texts) {
-            Thread thread = new Thread(() -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            });
-            threadList.add(thread);
-            thread.start();
+            futureList.add(executorService.submit(
+                    new MyCallable(text)
+            ));
         }
 
-        for(Thread thread: threadList) {
-            thread.join();
+        int maxSize = 0;
+        for(Future<Integer> future: futureList) {
+            maxSize = Math.max(future.get(), maxSize);
         }
 
         long endTs = System.currentTimeMillis(); // end time
